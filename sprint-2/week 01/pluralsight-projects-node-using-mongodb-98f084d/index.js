2c16e4d52bf52d0772a7c5f1813eaee4aeec80c6
Volodymyr Mahuran <97235802+Vovchyk-Mahuran@users.noreply.github.com>
2022-03-25 17:34:03 +0200

    Add files via upload

---
+++ sprint-2/week 01/pluralsight-projects-node-using-mongodb-98f084d/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import routes from './src/routes/routes';
+
+const app = express();
+const PORT = 4000;
+
+mongoose.Promise = global.Prompise;
+mongoose.connect('mongodb://localhost/productsdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+})
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+
+routes(app);
+
+app.get('/', (req, res) =>
+    res.send(`Store server running on port ${PORT}`)
+);
+
+app.listen(PORT, () => 
+    console.log(`Your server is running on port ${PORT}`)
+);
